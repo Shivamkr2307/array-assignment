41e2e7febd4eb7886209051d7c2df687271f4e7c
Shivamkr2307 <tiwaryshivam8@gmail.com>
2025-12-29 11:27:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let numbers = [10, 25, 30, 45, 50];
+let totalSum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    totalSum += numbers[i]; 
+}
+console.log("The values in the array are: " + numbers);
+console.log("The sum of all values is: " + totalSum);
